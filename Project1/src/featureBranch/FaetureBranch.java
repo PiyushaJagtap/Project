8385c79f7bd5f848e359d9298e43bb6a457f1450
swatihinge <swati.sawant35@gmail.com>
2022-05-23 15:57:44 +0530

    Second commit
    how are you

---
+++ Project1/src/featureBranch/FaetureBranch.java
@@ -0,0 +1,12 @@
+package featureBranch;
+
+public class FaetureBranch {
+
+	public static void main(String[] args) {
+		  System.out.println("Hii");
+		  System.out.println("How are you?");
+		  System.out.println("I am fine");
+
+	}
+
+}
